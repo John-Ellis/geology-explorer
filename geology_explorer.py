3d0045d75288eaabdd3ec748a742cb3b4af01885
john <Johnaellis0@gmail.com>
2018-10-28 12:32:22 -0700

    added live reload server.

--- geology_explorer.py
+++ geology_explorer.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from livereload import Server
 boreholeAPI = Flask(__name__)
 
 
@@ -9,9 +10,14 @@ def serve_location_selection():
 
 @boreholeAPI.route('/cutaway_view/<location>')
 def show_cutaway(location):
-  #TODO use location to get borehole data
+  # TODO use location to get borehole data
   bore_hole_data = {"lithological_data": ["blah", "blah", "blob"]}
   return render_template('cutaway_view.html', bore_hole_data=bore_hole_data)
 
+
+# Wrapper to enable live-reload
+boreholeAPI.debug = True
+server = Server(boreholeAPI)
+
 if __name__ == '__main__':
-  boreholeAPI.run()
+  server.serve()
